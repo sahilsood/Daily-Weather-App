3818f8989a139b7574c131d710459cbe01459223
Sahil Sood <sahil28sood@gmail.com>
2024-07-15 19:40:04 -0400

    added more dependencies

--- app/src/main/java/xyz/sahilsood/dailyweather/ui/HomeActivity.kt
+++ app/src/main/java/xyz/sahilsood/dailyweather/ui/HomeActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import xyz.sahilsood.dailyweather.R
 
 class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
