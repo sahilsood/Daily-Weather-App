06d3d99368227e3b9256a24037636daea02f8b53
Sahil Sood <sahil28sood@gmail.com>
2024-07-29 21:54:02 -0400

    added compose theme

--- app/src/main/java/xyz/sahilsood/dailyweather/ui/HomeActivity.kt
+++ app/src/main/java/xyz/sahilsood/dailyweather/ui/HomeActivity.kt
@@ -3,18 +3,26 @@ package xyz.sahilsood.dailyweather.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 import xyz.sahilsood.dailyweather.navigation.WeatherNavigation
+import xyz.sahilsood.dailyweather.ui.theme.DailyWeatherTheme
 
 @AndroidEntryPoint
 class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WeatherNavigation()        }
+            DailyWeatherTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    WeatherNavigation()
+                }
+            }
+        }
     }
 
     @Composable
