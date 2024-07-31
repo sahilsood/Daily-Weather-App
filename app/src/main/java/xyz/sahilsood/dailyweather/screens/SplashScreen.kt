e3f18b4c869ce4d1032b9793c2b426d596e8de0e
Sahil Sood <sahil28sood@gmail.com>
2024-07-30 23:10:19 -0400

    added splash screen

--- app/src/main/java/xyz/sahilsood/dailyweather/screens/SplashScreen.kt
+++ app/src/main/java/xyz/sahilsood/dailyweather/screens/SplashScreen.kt
@@ -1,10 +1,51 @@
 package xyz.sahilsood.dailyweather.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import xyz.sahilsood.dailyweather.R
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
-    Text(text = "Hello, World")
+    Surface(
+        modifier = Modifier
+            .padding(16.dp)
+            .size(330.dp),
+        shape = CircleShape,
+        color = Color.White,
+        border = BorderStroke(width = 2.dp, color = Color.LightGray)
+    ) {
+        Column(
+            modifier = Modifier.padding(1.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.sun),
+                contentDescription = "Sun",
+                modifier = Modifier.size(95.dp),
+                contentScale = ContentScale.Fit
+            )
+            Text(
+                text = "Find the Sun?",
+                style = MaterialTheme.typography.h5,
+                color = Color.LightGray
+            )
+        }
+    }
 }
--- app/src/main/java/xyz/sahilsood/dailyweather/ui/HomeActivity.kt
+++ app/src/main/java/xyz/sahilsood/dailyweather/ui/HomeActivity.kt
@@ -3,10 +3,15 @@ package xyz.sahilsood.dailyweather.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 import xyz.sahilsood.dailyweather.navigation.WeatherNavigation
@@ -18,8 +23,16 @@ class HomeActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DailyWeatherTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    WeatherNavigation()
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        WeatherNavigation()
+                    }
                 }
             }
         }
